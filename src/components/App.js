9a7ce6b6600837d4d253b01604301ebe0934b9f5
Amirandard <am.miranda.rd@gmail.com>
2021-10-01 19:16:38 +0200

    Adding filter

--- src/components/App.js
+++ src/components/App.js
@@ -2,28 +2,41 @@ import { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
 import callToApi from '../services/api';
 import CharachterList from './CharacterList';
+import Filters from './Filters';
+
 
 const App = () => {
   const [data, setData]=useState([]);
+  const [searchByName, setSearchByName]= useState('');
 
   useEffect(()=>{
     callToApi().then((initialData)=> {
-      console.log(initialData);
+      
       setData(initialData)
     })
-  }
+  },[])
   
-  ,[]) 
+  const handleSearchByName=(ev)=>{
+    setSearchByName(ev.currentTarget.value)
+    
+  }
+  const handleForm=(event)=>{
+    event.preventDefault()
+  }
+
+  const filteredData = data.filter((character) =>
+      character.name.toLocaleLowerCase().includes(searchByName.toLocaleLowerCase())
+    )
+  
   return (
-    <form>
+    <form onSubmit={handleForm}>
   <h1>Personajes de Rick y Morty</h1>
 
-  <input type="text" 
-  name='name'
-  value='control'
-  />
+<Filters searchByName={searchByName} 
+handleSearchByName={handleSearchByName}
+/>
 
-  <CharachterList data={data}/>
+  <CharachterList data={filteredData}/>
     
       
     </form>
---
+++ src/components/Filters.js
@@ -0,0 +1,15 @@
+const Filters =(props)=>{
+
+  
+  return( 
+ <input 
+  className='search-name'
+  type="text" 
+  name='name'
+  value={props.searchByName}
+  onChange={props.handleSearchByName}
+  />
+  )
+}
+
+export default Filters;
