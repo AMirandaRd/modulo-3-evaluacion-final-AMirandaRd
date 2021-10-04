a337c606c7bff306ed740a44054c042fb71d8004
Amirandard <am.miranda.rd@gmail.com>
2021-10-04 14:14:59 +0200

    change style

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,6 +1,8 @@
+import '../styles/Filters.scss'
+
 const Filters = (props) => {
   return (
-    <>
+    <section className='filters'>
       <input
       placeholder='Busca tu personaje favorito'
         className='search-name'
@@ -15,62 +17,67 @@ const Filters = (props) => {
       <option value="Alien">Alien</option>
       </select>
 
-      <label htmlFor="status"> 
-       Ha fallecido
-       <input type="radio"
-       name='status'
-       value="Dead"
-       onChange={props.handleSearchByStatus}
-       checked={props.searchByStatus==='Dead'}/>
-      
-       </label>
-       <label htmlFor="status">
-         Sigue vivo
+      <form action="" className='StatusSearcher'>
+        <p>Buscar solo personajes que...</p>
+        <label htmlFor="status">
+         Hayan fallecido
          <input type="radio"
          name='status'
-         value='Alive'
+         value="Dead"
          onChange={props.handleSearchByStatus}
-         checked={props.searchByStatus==='Alive'}
-         />
+         checked={props.searchByStatus==='Dead'}/>
         
-       </label>
-       <label htmlFor="status"> 
-       Desconocido
-       <input type="radio"
-       name='status'
-       value="unknown"
-       onChange={props.handleSearchByStatus}
-       checked={props.searchByStatus==='unknown'}/>
-       </label>
+         </label>
+         <label htmlFor="status">
+           Sigan vivos
+           <input type="radio"
+           name='status'
+           value='Alive'
+           onChange={props.handleSearchByStatus}
+           checked={props.searchByStatus==='Alive'}
+           />
+        
+         </label>
+         <label htmlFor="status">
+         Desconocido
+         <input type="radio"
+         name='status'
+         value="unknown"
+         onChange={props.handleSearchByStatus}
+         checked={props.searchByStatus==='unknown'}/>
+         </label>
+      </form>
 
-       <label htmlFor="gender"> 
-       Mujer
-       <input type="radio"
-       name='gender'
-       value="Female"
-       onChange={props.handleSearchByGender}
-       checked={props.searchByGender==='Female'}/>
-      
-       </label>
-       <label htmlFor="gender">
-         Hombre
+       <form action="" className='GenderSearcher'>
+         <p> Filtrar por sexo:</p>
+         <label htmlFor="gender">
+         Mujer
          <input type="radio"
          name='gender'
-         value='Male'
+         value="Female"
          onChange={props.handleSearchByGender}
-         checked={props.searchByGender==='Male'}
-         />
-        
-       </label>
-       <label htmlFor="status"> 
-       Desconocido
-       <input type="radio"
-       name='status'
-       value="unknown"
-       onChange={props.handleSearchByGender}
-       checked={props.searchByGender==='unknown'}/>
-       </label>
-    </>
+         checked={props.searchByGender==='Female'}/>
+         </label>
+         <label htmlFor="gender">
+           Hombre
+           <input type="radio"
+           name='gender'
+           value='Male'
+           onChange={props.handleSearchByGender}
+           checked={props.searchByGender==='Male'}
+           />
+         
+         </label>
+         <label htmlFor="status">
+         Desconocido
+         <input type="radio"
+         name='status'
+         value="unknown"
+         onChange={props.handleSearchByGender}
+         checked={props.searchByGender==='unknown'}/>
+         </label>
+       </form>
+    </section>
      );
 };
 
